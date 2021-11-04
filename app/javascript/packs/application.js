a479489dc36894aa552fe6f8145104cb00c32e2a
AbiezerSidonioJoseOdanacaJunior <abiezerodanacajosejunior@gmail.com>
2021-11-04 13:23:49 -0300

    Update Application

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,10 +12,12 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-//= require bootstrap
-//= require bootstrap_sb_admin_base_v2
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap_sb_admin_base_v2
+//= require turbolinks
+//= require_tree .
+
 
 
 
